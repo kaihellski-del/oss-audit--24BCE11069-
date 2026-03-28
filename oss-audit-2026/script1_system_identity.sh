e7a046aa60e0b5da9201edea25edc1cf34ed7633
kaihellski-del <kaihellski@gmail.com>
2026-03-29 02:02:08 +0530

    Update script1_system_identity.sh

--- oss-audit-2026/script1_system_identity.sh
+++ oss-audit-2026/script1_system_identity.sh
@@ -6,13 +6,13 @@ UPTIME=$(uptime -p)
 DISTRO=$(grep '^PRETTY_NAME' /etc/os-release | cut -d '"' -f 2)
 CURRENT_DATE=$(date '+%A, %B %d, %Y - %T')
 
-echo "======================================================"
+echo "----------------------------------------"
 echo "        OPEN SOURCE AUDIT - SYSTEM REPORT"
-echo "======================================================"
+echo "----------------------------------------"
 echo "User            : $USER_NAME"
 echo "Distribution    : $DISTRO"
 echo "Kernel Version  : $KERNEL"
 echo "System Uptime   : $UPTIME"
 echo "Current Date    : $CURRENT_DATE"
 echo "License         : GNU GPL v2 (Git)"
-echo "======================================================"
+echo "----------------------------------------"
